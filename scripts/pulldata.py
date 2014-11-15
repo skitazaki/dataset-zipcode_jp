877cb69a56f65d7f350846a2aa1dbb00ab087e28
Shigeru Kitazaki <skitazaki@gmail.com>
2014-11-15 17:46:33 +0900

    Follow the version up of "Data Package" specification to rename fields.

--- scripts/pulldata.py
+++ scripts/pulldata.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """Pull Zip archive, extract CSV file, and convert encoding into UTF-8.
 """
 
 import atexit
 import codecs
+import copy
 import logging
 import os
 import json
@@ -15,6 +17,14 @@ import unicodedata
 import zipfile
 from functools import partial
 
+APPNAME = os.path.splitext(os.path.basename(__file__))[0]
+VERSION = '0.2.0'
+
+LOG_FORMAT = '%(asctime)s|%(levelname)s|%(message)s'
+LOG_FORMAT_DEBUG = '%(asctime)s | %(name)s:%(levelname)s | ' + \
+                   '%(filename)s:%(lineno)d | %(message)s'
+LOG_DATEFMT = '%Y-%m-%d %H:%M:%S'
+
 ORIGINAL_ENCODING = 'cp932'  # Downloaded file encoding
 ENCODING = 'utf-8'  # Follow "Open Data Protocol"
 NORMALIZATION_FORM = 'NFKC'
@@ -22,30 +32,23 @@ BASEDIR = os.path.join(os.path.dirname(__file__), '..')
 DATA_DIR = os.path.join(BASEDIR, 'data')
 DATA_PACKAGE = os.path.join(BASEDIR, 'datapackage.json')
 
+REMOTE_DIR = "http://www.post.japanpost.jp/zipcode/dl"
 RESOURCES = (
     {
-        "title": "Zip package (Oogaki)",
         "name": "ken_all_oogaki",
-        "path": "data/ken_all_oogaki.csv",
-        "url": "http://www.post.japanpost.jp/zipcode/dl/oogaki/zip/ken_all.zip"
+        "url": REMOTE_DIR + "/oogaki/zip/ken_all.zip"
     },
     {
-        "title": "Zip package (Kogaki)",
         "name": "ken_all_kogaki",
-        "path": "data/ken_all_kogaki.csv",
-        "url": "http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip"
+        "url": REMOTE_DIR + "/kogaki/zip/ken_all.zip"
     },
     {
-        "title": "Zip package (Roman)",
         "name": "ken_all_roman",
-        "path": "data/ken_all_roman.csv",
-        "url": "http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip"
+        "url": REMOTE_DIR + "/roman/ken_all_rome.zip"
     },
     {
-        "title": "Zip package (Facility)",
         "name": "facility",
-        "path": "data/facility.csv",
-        "url": "http://www.post.japanpost.jp/zipcode/dl/jigyosyo/zip/jigyosyo.zip"
+        "url": REMOTE_DIR + "/jigyosyo/zip/jigyosyo.zip"
     }
 )
 
@@ -68,7 +71,7 @@ class Event(object):
                 r = callback(r)
                 if r is None:
                     return
-            except Exception, e:
+            except Exception as e:
                 for errback in self.errbacks:
                     errback(e)
                 return
@@ -76,72 +79,99 @@ class Event(object):
 
 class ZipDownloadUnpack(object):
 
-    def __init__(self, datadir, cachedir):
-        self.datadir = datadir
-        self.cachedir = cachedir
+    def __init__(self, basedir):
+        self.basedir = basedir
+        self.logger = logging.getLogger(APPNAME)
+        self.cachedir = tempfile.mkdtemp()
+        atexit.register(shutil.rmtree, self.cachedir)
+        self.logger.debug('Created cache directory: %s', self.cachedir)
 
-    def process(self, resource, fields):
-        logging.info('Start processing: %s', resource['name'])
+    def process(self, resource):
+        self.logger.info('Start processing: %s', resource['name'])
         evt = Event()
         evt.callback(self.download)
         evt.callback(self.unpack)
-        self.target = os.path.join(self.datadir, resource['name'] + '.csv')
-        self.fields = fields
-        evt.callback(self.write_header)
         evt.callback(self.convert)
-        evt.errback(logging.error)
+        evt.callback(self.write)
+        evt.errback(self.logger.error)
         evt.fire(resource)
-        logging.info('End processing: %s', resource['name'])
+        self.logger.info('End processing: %s', resource['name'])
 
     def download(self, resource):
         path = os.path.join(self.cachedir, resource['name'] + '.zip')
         if os.path.exists(path):
-            logging.debug('%s already exists.', resource['name'])
+            self.logger.debug('"%s" is already downloaded.', resource['name'])
             return
-        try:
-            urllib.urlretrieve(resource['url'], path)
-        except IOError:
-            logging.error('Fail to download from: %s', resource['url'])
-        else:
-            logging.info('Successfully downloaded: %s', resource['url'])
-            return path
+        self.logger.info('Start downloading: %s -> %s', resource['url'], path)
+        urllib.urlretrieve(resource['url'], path)
+        self.logger.info('Successfully downloaded "{}" {:,} bytes.'.format(
+            os.path.basename(path), os.path.getsize(path)))
+        return resource
 
-    def unpack(self, zipname):
-        with zipfile.ZipFile(zipname) as z:
+    def unpack(self, resource):
+        path = os.path.join(self.cachedir, resource['name'] + '.zip')
+        self.logger.debug('Unpacking ZIP file: %s', path)
+        with zipfile.ZipFile(path) as z:
             for f in z.namelist():
-                if f.endswith(('.csv', '.CSV')):
+                if f.lower().endswith('.csv'):
                     z.extract(f, self.cachedir)
-                    return os.path.join(self.cachedir, f)
+                    resource['_local'] = os.path.join(self.cachedir, f)
+                    return resource
 
-    def write_header(self, csvname):
-        with codecs.open(self.target, 'w', ENCODING) as w:
-            w.write(','.join(tuple(map(lambda f: f['id'], self.fields))))
-            w.write('\n')
-        return csvname
-
-    def convert(self, csvname):
+    def convert(self, resource):
         # Convert half-width Katakana to full-width
+        path = resource['_local']
+        self.logger.debug('Converting CSV file: %s', path)
+        data = []
         normalize = partial(unicodedata.normalize, NORMALIZATION_FORM)
-        with codecs.open(csvname, 'r', ORIGINAL_ENCODING) as r:
-            with codecs.open(self.target, 'a', ENCODING) as w:
-                [w.write(normalize(row)) for row in r]
+        with codecs.open(path, 'r', ORIGINAL_ENCODING) as fp:
+            for line in fp:
+                data.append(normalize(line.rstrip('\r\n')))
+        resource['data'] = data
+        return resource
+
+    def write(self, resource):
+        path = os.path.join(self.basedir, resource['path'])
+        encoding = resource.get('encoding', ENCODING)
+        fields = resource['schema']['fields']
+        data = resource['data']
+        with codecs.open(path, 'w', encoding) as w:
+            header = tuple(map(lambda f: f['name'], fields))
+            w.write(','.join(header))
+            w.write('\n')
+            w.write('\n'.join(data))
+            w.write('\n')
+        self.logger.info('Wrote "{}" {:,} bytes of {:,} lines.'.format(
+            resource['path'], os.path.getsize(path), len(data)))
+
+
+def setup_logger(verbosity=logging.INFO):
+    logger = logging.getLogger(APPNAME)
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    handler.setLevel(verbosity)
+    fmt = LOG_FORMAT_DEBUG if verbosity == logging.DEBUG else LOG_FORMAT
+    handler.setFormatter(logging.Formatter(fmt, datefmt=LOG_DATEFMT))
+    logger.addHandler(handler)
+    return logger
 
 
 def main():
-    cache = tempfile.mkdtemp()
-    atexit.register(shutil.rmtree, cache)
+    logger = setup_logger()
     if not os.path.exists(DATA_DIR):
         os.makedirs(DATA_DIR)
     with open(DATA_PACKAGE) as fp:
         package = json.load(fp)
     resources = package['resources']
-    processor = ZipDownloadUnpack(DATA_DIR, cache)
+    processor = ZipDownloadUnpack(BASEDIR)
     for r in RESOURCES:
-        s = filter(lambda t: t['path'] == r['path'], resources)
-        if not s:
-            logging.error('"%s" is not found on datapackage to compare path attribute.', r['title'])
-            continue
-        processor.process(r, s[0]['schema']['fields'])
+        s = tuple(filter(lambda t: t['name'] == r['name'], resources))
+        if s:
+            a = copy.copy(s[0])
+            a['url'] = r['url']
+            processor.process(a)
+        else:
+            logger.error('"%s" is not found on datapackage.', r['name'])
 
 
 if __name__ == '__main__':
